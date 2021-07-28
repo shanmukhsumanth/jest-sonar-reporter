90a9f82b8454699f5a848bb99f00ce4aba4482ea
KalpanaAddagada <42040121+KalpanaAddagada@users.noreply.github.com>
2021-07-28 16:46:35 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -3,10 +3,22 @@ const sonarqubeScanner = require('sonarqube-scanner');
 sonarqubeScanner({
   serverUrl: 'http://18.223.255.187:9000/',
        options : {
-       'sonar.sources': '.',
-       'sonar.inclusions' : '**',
+       'sonar.projectKey': 'node-hello',
+       'sonar.projectName': 'node-hello',
+       'sonar.language':'js',
+       'sonar.sources':'lib',
+       'sonar.sourceEncoding':'UTF-8',
+       'sonar.projectVersion':'2.0',
+       'sonar.tests':'lib',
        
+       'sonar.sources': 'lib',
+       'sonar.test.inclusions': 'lib/**/__tests__/*.spec.js',
+       'sonar.coverage.exclusions': 'lib/**/__tests__/**,lib/**/__snapshots__/**',
+       'sonar.javascript.jstest.reportsPath': 'site/report',
 
-//        'sonar.coverageReportPaths' : './Junit.xml'
+       'sonar.testExecutionReportPaths': 'reports/test-report.xml',
+       'sonar.javascript.coveragePlugin': 'lcov',
+       'sonar.javascript.lcov.reportPath' : './coverage/lcov.info'
+
        },
 }, () => {});
